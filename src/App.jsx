167898aa9de9467e9d5dc8e369b02e0ac113512c
workwithA <workabbasbayat@gmail.com>
2024-06-01 20:36:04 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
     <div className="w-screen h-screen flex justify-center items-center flex-col bg-black">
       <div className="w-96 h-96 rounded-full shadow-xl shadow-gray-500 flex justify-center items-center flex-col">
         <h1 className="text-4xl font-mono text-white">Coming Soon ...</h1>
-        <p className="text-white text-center w-11/12 mt-5 opacity-70">the personal webiste of <span className="animate-pulse text-lg">Alireza Tat</span> will be released soon</p>
+        <p className="text-white text-center w-11/12 mt-5 opacity-70">the personal webiste of <span className="animate-pulse text-lg font-bold">Alireza Tat</span> will be released soon</p>
       </div>
     </div>
   )
